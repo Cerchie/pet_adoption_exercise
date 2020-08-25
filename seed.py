7227d223870ba430e20d83640f23ac2ae0500a87
Lucia Cerchie <luciacerchie@gmail.com>
2020-08-25 11:22:46 -0700

    img issue

--- seed.py
+++ seed.py
@@ -5,7 +5,7 @@ db.drop_all()
 db.create_all()
 
 p = Pet(name="Jane", photo_url="", available=True)
-p2 = Pet(name="Janet", photo_url="https://assets.petco.com/petco/image/upload/f_auto,q_auto/cathp-092619-img-new-pet-adult-256w-256h-d", available="True")
+p2 = Pet(name="Janet", photo_url="https://assets.petco.com/petco/image/upload/f_auto,q_auto/cathp-092619-img-new-pet-adult-256w-256h-d", available=True)
 p3 = Pet(name="Jan", photo_url="", available=False)
 db.session.add(p)
 db.session.add(p2)
